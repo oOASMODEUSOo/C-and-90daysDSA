bf1a71eb5bb916d58de5a8777e29c2e6ff7b9de6
oOASMODEUSOo <allforyt96@gmail.com>
2022-02-23 23:41:39 +0530

    Function question

--- C coding/3 C functions/4sumOfNaturalNum.c
+++ C coding/3 C functions/4sumOfNaturalNum.c
@@ -1,2 +1,25 @@
 //C Program to Find the Sum of Natural Numbers using Recursion
 
+#include <stdio.h>
+
+int SumCalc(int num){
+
+    if (num != 0){
+        return num + SumCalc(num-1);
+    }
+    else{
+        return num;
+    }
+}
+
+int main(){
+
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int final = SumCalc(num);
+    printf("%d", final);
+
+    return 0;
+}
