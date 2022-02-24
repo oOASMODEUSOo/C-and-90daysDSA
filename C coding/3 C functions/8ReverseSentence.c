15e8cecbe7200e7b31df4b20b51d635d3df0d707
oOASMODEUSOo <allforyt96@gmail.com>
2022-02-24 23:18:09 +0530

    ye delete kyu ni hora

--- C coding/3 C functions/8ReverseSentence.c
+++ C coding/3 C functions/8ReverseSentence.c
@@ -12,7 +12,8 @@ void reverseSentence() {
     char c;
     scanf("%c", &c);
     if (c != '\n') {
-        reverseSentence();
+        printf("%u\n", &c);
+        reverseSentence(); //bsdk ka function pause ho jata hai yahan pe jab tk vo if block false ni ho jata
         printf("%c", c);
     }
 }
--- C coding/tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
-    // printf("Adress se value nikalna hai to %d\n", *addressOf);
-    // printf("Value of variable %d and adress of variable %d\n", &something, addressOf);
-    // printf("Value of variable %d and adress of variable %u\n", something, addressOf);
-    // printf("Adress of adress %u", &addressOf);
