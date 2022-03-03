7ef75a4f4d9f0e8e713fd11c189b25d32e36a20c
oOASMODEUSOo <allforyt96@gmail.com>
2022-03-04 00:55:15 +0530

    isse zada nai hoga

--- C coding/TestingCode.c
+++ C coding/TestingCode.c
@@ -1,42 +1,19 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdio.h> 
 
-// int main(){
-
-//     char str[50];
-//     printf("enter a string: ");
-//     gets(str);
-//     puts(str);
-//     int i=0;
-//     while (str[i] != '\0'){
-//         i++;
-//     }
-//     printf("%d", i);
-// }
-
-
-// int main(){
-//     int a = 3;
-//     printf("%d %d %d", a, ++a, a++);
-//     return 0;
-// }
-
-int main(){
-    int something = 5;
-    int *addressOf = &something;   
-
-//* means value of and & means adress off
-
-    printf("Adress se value nikalna hai to %d\n", *addressOf);
-    printf("Value of variable %d and adress of variable %d\n", &something, addressOf);
-    printf("Value of variable %d and adress of variable %u\n", something, addressOf);
-    printf("Adress of adress %u", &addressOf);
-
-    printf("%c\n", '%');
-
-    printf("%d\n", 5%2);
-    printf("%d\n", -5%2);
-    printf("%d\n", 5%-2);
+int fact(int n){
+    if (n>=1){
+        return n*fact(n-1);
+    }
+    else{
+        return 1;
+    }
 }
 
-
+int main(){
+    int n;
+    printf("Enter a number: \n");
+    scanf("%d\n", &n);
+    
+    printf("%d", fact(n));
+    return 0;
+}
