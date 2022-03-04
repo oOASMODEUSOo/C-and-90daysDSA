0ef269c403d4dc55ab50013f4448799e0dfe1237
oOASMODEUSOo <allforyt96@gmail.com>
2022-03-04 19:58:03 +0530

    pta ni kya kia hu

--- C coding/TestingCode.c
+++ C coding/TestingCode.c
@@ -1,19 +1,7 @@
 #include <stdio.h> 
 
-int fact(int n){
-    if (n>=1){
-        return n*fact(n-1);
-    }
-    else{
-        return 1;
-    }
-}
-
 int main(){
-    int n;
-    printf("Enter a number: \n");
-    scanf("%d\n", &n);
     
-    printf("%d", fact(n));
+    
     return 0;
 }
